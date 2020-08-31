c6a8200bdf14001adfec8f027fc6f9c98ec83fb5
Sayantan-codes <sayantan.jr15@gmail.com>
2020-08-31 22:23:17 +0530

    added files

---
+++ Test_file.py
@@ -0,0 +1,3 @@
+'''A python news Aggregator Project by Gaurav, Sayantan and Suhail.'''
+print("Welcome to our project, do contribute and spread the social-coding culture.")
+print("------------------------HAVE A NICE DAY--------------------")
