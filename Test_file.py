c17650a8fff0ceaf303c774ec6db96e49d00aab1
Syed Suhail Salaha <scube537@gmail.com>
2020-09-01 09:22:24 +0530

    added three technologies used

--- Test_file.py
+++ Test_file.py
@@ -6,3 +6,7 @@ and sending scheduled emails to their respectove email IDs.
 '''
 print("Welcome to our project, do contribute and spread the social-coding culture.")
 print("------------------------HAVE A NICE DAY--------------------")
+print('''Technologies used: 
+\n-->google search api 
+\n-->natural language processing 
+\n-->selenium webdriver''')
