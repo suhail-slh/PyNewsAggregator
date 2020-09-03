32bab8366fd3c04c33fa35db2ad436aeef8257c5
Syed Suhail Salaha <50571695+suhail-slh@users.noreply.github.com>
2020-09-03 11:51:17 +0530

    In GUI.py 	-A new object is initialized everytime there is a subscribe/unsubscribe action In account_manager.py 	-Created a save function to save changes to the user database 	-An object's "mail_id" and "keywords" attributes will now be set at initialization and not at the subscribe/unsubscribe actions In keyword_processor.py 	-Replaced the googlesearch and newspaper modules with the GoogleNews module 	-The most relevant site will be chosen according to the highest frequency of keyword occurence

--- account_manager.py
+++ account_manager.py
@@ -2,10 +2,9 @@ from keyword_processor import KeywordProcessor
 try:
 	import pandas as pd
 except:
-	print("module pandas not installed")
+	print("missing module: pandas")
 
-src = r"user_database.csv" #add the path to the user database
-src2 = r"" #add the path to the directory where the links will be stored
+src = r"C:\Users\mohammed\Documents\Projects\tests\user_database.csv" #add the path to the user database
 
 try:        
 	f = open(src, 'x') 
@@ -17,42 +16,39 @@ except FileExistsError:
 
 df = pd.DataFrame(database)
 
+def save():
+	with open(src, 'w') as f:
+		df.to_csv(f, index = False)
+
 class AccountManager:
-	def __init__(self):
-		self.email_id = ''
-		self.keywords = ''
-		self.links = []
-	
-	def subscribe(self,email_id,keywords):
+	def __init__(self,email_id,keywords):
 		self.email_id = email_id.strip()
 		self.keywords = keywords.strip()
-		
+		self.links = []
+	
+	def subscribe(self):
 		global df
+		self.unsubscribe() #to overwrite account preferences
 		df = df.append({'Email ID':self.email_id,'Keywords':self.keywords}, ignore_index=True)
-		df.dropna(inplace = True)
 		
-		with open(src, 'w') as f:
-			df.to_csv(f, index = False)
+		save()
 			
 		self.get_links()
 		
-		with open(src2+"\\"+self.email_id.split('@')[0]+".txt", "a") as f:
-			for link in self.links:
-				f.write(link+",")
+		#replace with send_mail function
+		print("Keywords:",self.keywords)
+		for link in self.links:
+			print(link)
 			
-	def unsubscribe(self,email_id):
+	def unsubscribe(self):
 		global df
-		filter = df["Email ID"]!=email_id.strip()
+		filter = df["Email ID"]!=self.email_id
 		df.where(filter, inplace = True)
 		df.dropna(inplace = True)
 		
-		with open(src, 'w') as f:
-			df.to_csv(f, index = False)
+		save()
 			
 	def get_links(self):
 		for keyword in self.keywords.split(','):
 			KP = KeywordProcessor(keyword)
 			self.links.append(KP.most_relevant())
-		
-	def __str__(self):
-		pass
