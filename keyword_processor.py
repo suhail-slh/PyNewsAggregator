a536164e5b3f74b6435d9b07559429394ae1dd66
suhail-slh <50571695+suhail-slh@users.noreply.github.com>
2020-09-05 09:45:00 +0530

    keyword_processor added
    Fetches links from at most a day ago
    Chooses the most relevant link based on
      -the number of distinct keywords present(greater is better)
      -the age of the article(younger is better)
    Returns the article

--- keyword_processor.py
+++ keyword_processor.py
@@ -68,10 +68,7 @@ class KeywordProcessor:
                 article.parse()
                 matches = re.findall(pattern, article.text, re.IGNORECASE)
                 matches = set([x.casefold() for x in matches])
-                try:
-                    age = get_age(link.get('date', ''))
-                except:
-                    pass
+                age = get_age(link.get('date', ''))
                 print('Current article age:', age, 'mins\nBest article age:', min_age, 'mins\nMatches:', matches,
                       '\nLink:', link.get('link', ''), '\n')
                 if len(matches) >= max_matches and age <= min_age:
@@ -84,4 +81,3 @@ class KeywordProcessor:
         most_relevant_article.download()
         most_relevant_article.parse()
         return most_relevant_article
-    
